df2294bc18865ebccf4685b09609dd71c19fbbed
milicamisic <milicamisic2303@gmail.com>
2023-11-28 13:36:24 +0100

    [ADD] Added accommodation card component.
    problem: won't load the image.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTableModule } from "@angular/material/table";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserManagementModule } from './user-management/user-management.module';
+import { AccommodationCardComponent } from './accommodation-management/accommodation-card/accommodation-card.component';
+import { HomePageComponent } from './accommodation-management/home-page/home-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccommodationCardComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
     BrowserAnimationsModule,
-    UserManagementModule
+    UserManagementModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
