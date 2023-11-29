f7d1057fa1f0376faa986ab71d4f3d38c740fa24
Sonja-Parabucki <105170152+Sonja-Parabucki@users.noreply.github.com>
2023-11-29 21:37:15 +0100

    validation added

--- src/app/user-management/change-password/change-password.component.ts
+++ src/app/user-management/change-password/change-password.component.ts
@@ -1,5 +1,25 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+
+const passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const passwordControl = control.get('newPassword');
+  const confirmPasswordControl = control.get('repeatPassword');
+
+  if (passwordControl && confirmPasswordControl && passwordControl.value !== confirmPasswordControl.value) {
+    if (confirmPasswordControl.errors) {
+      confirmPasswordControl.setErrors({ ...confirmPasswordControl.errors, passwordMismatch: true });
+    } else {
+      confirmPasswordControl.setErrors({ passwordMismatch: true });
+    }
+    return { passwordMismatch: true };
+  } else {
+    if (confirmPasswordControl) {
+      confirmPasswordControl.setErrors(null);
+    }
+    return null;
+  }
+};
 
 @Component({
   selector: 'app-change-password',
@@ -20,7 +40,7 @@ export class ChangePasswordComponent {
       oldPassword: ['', Validators.required],
       newPassword: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,20}$/)]],
       repeatPassword: ['', Validators.required]
-    })
+    }, {validator: passwordMatchValidator});
   }
 
 
