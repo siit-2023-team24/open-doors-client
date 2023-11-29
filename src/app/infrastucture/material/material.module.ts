703137592240ffb139c0da5d215fb6bc12a95fba
Vasilije Marković <markovic.sv15.2021@uns.ac.rs>
2023-11-29 20:08:06 +0100

    Added Angular material to register components.

--- src/app/infrastucture/material/material.module.ts
+++ src/app/infrastucture/material/material.module.ts
@@ -4,6 +4,7 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule, MatLabel} from "@angular/material/form-field";
 import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio'
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    MatRadioModule
   ],
 
   exports: [
@@ -22,7 +24,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatInputModule,
     MatLabel,
-    MatSelectModule
+    MatSelectModule,
+    MatRadioModule
   ]
 
 })
