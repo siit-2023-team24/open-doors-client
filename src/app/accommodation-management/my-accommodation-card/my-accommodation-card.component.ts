4b6f51ed22d4f210482e34f1aa7f71d19f49e269
Sonja-Parabucki <105170152+Sonja-Parabucki@users.noreply.github.com>
2023-12-13 18:29:51 +0100

    host's accommodations implemented

--- src/app/accommodation-management/my-accommodation-card/my-accommodation-card.component.ts
+++ src/app/accommodation-management/my-accommodation-card/my-accommodation-card.component.ts
@@ -1,14 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { HostListAccommodation } from '../model/host-list-accommodation';
+import { ImageService } from 'src/app/image-management/image.service';
 
 @Component({
   selector: 'app-my-accommodation-card',
   templateUrl: './my-accommodation-card.component.html',
-  styleUrls: ['./my-accommodation-card.component.css']
+  styleUrls: ['./my-accommodation-card.component.css', '../../../styles.css']
 })
 export class MyAccommodationCardComponent {
 
+
   @Input()
   accommodation: HostListAccommodation;
+
+  imagePath: string;
+
+  constructor(private imageService: ImageService) {
+  }
+
+  ngOnInit() {
+    this.imagePath = this.imageService.getPath(this.accommodation.image, false);
+  }
 
 }
