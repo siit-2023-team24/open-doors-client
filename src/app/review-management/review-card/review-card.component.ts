ac95656de105c59eaae2eef59a51a9c24cc311df
Vasilije Marković <markovic.sv15.2021@uns.ac.rs>
2024-01-09 23:46:47 +0100

    Guest is enabled to delete their own reviews

--- src/app/review-management/review-card/review-card.component.ts
+++ src/app/review-management/review-card/review-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ReviewDetailsDTO } from '../model/review-details';
 import { ImageService } from 'src/app/image-management/image.service';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-review-card',
@@ -11,9 +12,14 @@ export class ReviewCardComponent {
   @Input() review: ReviewDetailsDTO;
 
   imagePath: string = "";
+  guest: string = "";
 
-  constructor(private imageService: ImageService) {}
+  constructor(private imageService: ImageService,
+              private authService: AuthService) {}
   ngOnInit(): void {
+    if (this.authService.isLoggedIn()) {
+      this.guest = this.authService.getUsername();
+    }
     this.imagePath = this.imageService.getPath(this.review.imageId, true);
   }
 }
